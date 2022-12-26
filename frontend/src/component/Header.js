8cdfa5f740c9fa4699af3ad510ffc1dc13dae87d
zni94 <87021422+zni94@users.noreply.github.com>
2022-12-26 16:01:37 +0900

    Update Authors

---
+++ frontend/src/component/Header.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header>
+        <Nav />
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ frontend/src/page/Gallery.js
@@ -0,0 +1,6 @@
+import '../lib/css/page/gallery.css';
+const Gallery = () =>{
+	return <div id={'gallery'}>GALLERY</div>
+}
+
+export default Gallery;
